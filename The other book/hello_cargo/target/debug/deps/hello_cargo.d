285146017c70b4fc24618656b1db6f3a31311de9
Jokkarn <kjellsta2013@gmail.com>
2023-04-04 17:49:42 +0200

    fix rust things

---
+++ The other book/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\uni\prog2006\bookReading\The other book\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\uni\prog2006\bookReading\The other book\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ The other book/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\uni\prog2006\bookReading\The\ other\ book\hello_cargo\target\debug\hello_cargo.exe: C:\uni\prog2006\bookReading\The\ other\ book\hello_cargo\src\main.rs
